ec7b39e838c6250df381e66bbe595259a9451113
Mark Bort Tomas <maboto01@xuqueralumnat.es>
2021-01-13 20:24:28 +0100

    PE1: Conexion a BBDD (en proceso)

--- src/assets/js/UsuariosView.js
+++ src/assets/js/UsuariosView.js
@@ -2,60 +2,54 @@ import '../css/UsuariosView.css';
 import { Component } from 'react';
 import { DataTable, Column } from 'primereact/datatable';
 import { Button } from 'primereact/button';
+import axios from 'axios';
 
 class UsuariosView extends Component {
-    render() {
-        const users = [
-            {
-                "UsuarioId":"Usuario1",
-                "Nombre":"Mark",
-                "Apellidos":"Bort Tomás",
-                "Edad":19,
-            },
-            {
-                "UsuarioId":"Usuario2",
-                "Nombre":"Pumpushita",
-                "Apellidos":"Asensi Muñoz",
-                "Edad":20
-            },
-            {
-                "UsuarioId":"Usuario3",
-                "Nombre":"Joel",
-                "Apellidos":"Bort Tomás",
-                "Edad":2
-            },
-            {
-                "UsuarioId":"Usuario4",
-                "Nombre":"Ruth",
-                "Apellidos":"Bort Lopez-Montenegro",
-                "Edad":8
-            }
-        ];
-
-        const deleteUserBtn = (rowData) => {
-            return <button onClick={handleClick}>{rowData.inventoryStatus}</button>
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
         }
+    }
+    
+    render() {
 
-        const handleClick = (e) => {
-            //e.preventDefault();
-            console.log("Usuario ID = " + JSON.stringify(e));
-          }
+            
+        
+        
 
+        // const deleteUserBtn = (rowData) => {
+        //     return <button onClick={handleClick}>{rowData.inventoryStatus}</button>
+        // }
+
+        // const handleClick = (e) => {
+        //     //e.preventDefault();
+        //     console.log("Usuario ID = " + JSON.stringify(e));
+        // }
+
+        // this.state.users.map(user => console.log(user.UsuarioId));
 
         return ( 
             <div>
-                <DataTable value={users}>
+                <DataTable value={this.state.users}>
                     <Column field="UsuarioId" header="UsuarioId"></Column>
                     <Column field="Nombre" header="Nombre"></Column>
                     <Column field="Apellidos" header="Apellidos"></Column>
                     <Column field="Edad" header="Edad"></Column>
-                    <Column header="Borrar" body={
-                        deleteUserBtn
-                    }></Column>
                 </DataTable>
+                <Button onClick={this.array}>kk</Button>
             </div>
         );
     }
-}
 
+    array = () => {
+        axios
+        .get('https://localhost:44342/api/Usuarios/')
+        .then(function(res) {
+            //console.log(res.data);
+            this.setState({users: res.data}, () => console.log(this.state.users));
+        });
+    }
+}
 export default UsuariosView;
