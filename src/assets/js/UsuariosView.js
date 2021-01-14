de7cefe4632bec8238de54a29a8d87850f07c7eb
Mark <bortmarc8@gmail.com>
2021-01-14 18:57:07 +0100

    Users: Añadido borrar, filtrar y ordenar usuarios, empezado cambiar contraseña

--- src/assets/js/UsuariosView.js
+++ src/assets/js/UsuariosView.js
@@ -1,6 +1,7 @@
 import '../css/UsuariosView.css';
 import { Component } from 'react';
-import { DataTable, Column } from 'primereact/datatable';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
 import axios from 'axios';
 
@@ -9,47 +10,122 @@ class UsuariosView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: []
+            users: [],
+            userAlterPwd: ""
         }
     }
-    
+  
     render() {
-
-            
         
+        const queryGetDatos = () => {
+            axios
+            .get('https://localhost:44342/api/Usuarios/')
+            .then(res => {
+                //console.log(res.data);
+                this.setState({users: res.data}, () => console.log(res.data));
+            });
+        }
+
+        const queryPostDatos = () => {
+            axios
+            .post('https:sample-endpoint.com/user', {
+                Name: 'Fred',
+                Age: '23'
+            })
+            .then(function (response) {
+                console.log(response);
+            });
+        }
+
+        const queryModPwd = (user, lastpwd, newpwd, repeatedNewPwd) => {
+            if (lastpwd !== newpwd && newpwd === repeatedNewPwd) {
+                axios
+                .post('https://localhost:44342/api/Account/ChangePassword?UsuarioId='+user, {
+                    "OldPassword": lastpwd,
+                    "NewPassword": newpwd,
+                    "ConfirmPassword": repeatedNewPwd
+                  })
+                .then(function (response) {
+                    console.log("Se ha modificado la contraseña de " + user + " que era " + lastpwd + " a " + newpwd);
+                });
+            } else {
+                console.log("NO SE HA MODIFICADO la contraseña de " + user + " que era " + lastpwd + " a " + newpwd);
+            }
+        }
+
+        const queryDeleteDatos = (id) => {
+            axios
+            .delete('https://localhost:44342/api/Usuarios/?id=' + id)
+            .then(response => {
+                queryGetDatos();
+            });
+        }
+
+        const UserBtns = (rowData) => {
+            return (<div>
+                        <button className="btn-delete-user" onClick={()=>handleDeleteClick(rowData)}>X</button>
+                        <div style={{height: '5px'}}></div>
+                        <button className="btn-modify-pwd" onClick={()=>handlePwdChangeClick(rowData)}>P</button>
+                    </div>);
+        }
+
+        const handleDeleteClick = (rowData) => {
+            if (window.confirm("¿Realmente quieres eliminar el usuario: " + rowData.UsuarioId + "? \n Se borrará de forma permanente")) {
+                console.log("Eliminado usuario: " + rowData.UsuarioId); 
+                queryDeleteDatos(rowData.UsuarioId)
+            } 
+        }
         
+        const handlePwdChangeClick = (rowData) => {
+            console.log(rowData);
+            console.log("Quiere cambiar su pwd");
+            if (document.getElementsByClassName("changePwdDialog")[0].style.display == "flex") {
+                document.getElementsByClassName("changePwdDialog")[0].style.display = "none";
+                document.getElementsByClassName("pwd_input")[0].value = "";
+                document.getElementsByClassName("pwd_input")[1].value = "";
+                document.getElementsByClassName("pwd_input")[2].value = "";
+                this.setState({userAlterPwd: undefined})
 
-        // const deleteUserBtn = (rowData) => {
-        //     return <button onClick={handleClick}>{rowData.inventoryStatus}</button>
-        // }
+            } else {
+                document.getElementsByClassName("changePwdDialog")[0].style.display = "flex";
+                this.setState({userAlterPwd: rowData.UsuarioId})
+            }
+        }
 
-        // const handleClick = (e) => {
-        //     //e.preventDefault();
-        //     console.log("Usuario ID = " + JSON.stringify(e));
-        // }
-
-        // this.state.users.map(user => console.log(user.UsuarioId));
+        queryGetDatos();
 
         return ( 
             <div>
-                <DataTable value={this.state.users}>
-                    <Column field="UsuarioId" header="UsuarioId"></Column>
-                    <Column field="Nombre" header="Nombre"></Column>
-                    <Column field="Apellidos" header="Apellidos"></Column>
-                    <Column field="Edad" header="Edad"></Column>
-                </DataTable>
-                <Button onClick={this.array}>kk</Button>
+                <div className="table-wrapper">
+                    <div className="fl-table">
+                        <DataTable class="fl-table" value={this.state.users}>
+                            <Column sortable={true} field="UsuarioId" header="ID Usuario" filter filterPlaceholder="UserID"></Column>
+                            <Column sortable={true} field="Nombre" header="Nombre" filter filterPlaceholder="Nombre"></Column>
+                            <Column sortable={true} field="Apellidos" header="Apellidos" filter filterPlaceholder="Apellidos"></Column>
+                            <Column sortable={true} field="Edad" header="Edad" filter filterType="Numeric" filterPlaceholder="Edad"></Column>
+                            <Column header="Acciones" body={UserBtns}></Column>
+                        </DataTable>
+                    </div>
+                </div> 
+                <div className="changePwdDialog">
+                    <h1>Modificar contraseña</h1>
+                    <br>
+                    </br>
+                    <input id="lastPwd" className="pwd_input" type="password" placeholder="Contraseña antigua"></input>
+                    <input id="newPwd" className="pwd_input" type="password" placeholder="Nueva contraseña"></input>
+                    <input id="repeatNewPwd" className="pwd_input" type="password" placeholder="Repetir nueva contraseña"></input>
+                    <br></br>
+                    <div style={{display: 'flex'}}>
+                        <button onClick={()=>queryModPwd(this.state.userAlterPwd,
+                                                        document.getElementById("lastPwd").value, 
+                                                        document.getElementById("newPwd").value, 
+                                                        document.getElementById("repeatNewPwd").value)}>Aceptar</button>
+                        <div style={{width: "25px"}}></div>
+                        <button onClick={()=>handlePwdChangeClick(0)}>Cancelar</button>
+                    </div>
+                </div>
             </div>
         );
-    }
-
-    array = () => {
-        axios
-        .get('https://localhost:44342/api/Usuarios/')
-        .then(function(res) {
-            //console.log(res.data);
-            this.setState({users: res.data}, () => console.log(this.state.users));
-        });
     }
 }
 export default UsuariosView;
